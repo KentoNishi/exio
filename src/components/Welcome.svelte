9a65b25fafa5ae729584b87d0b8a03631893014c
Kento Nishi <kento24gs@outlook.com>
2022-04-05 07:07:53 +0000

    limit effects when holding

--- src/components/Welcome.svelte
+++ src/components/Welcome.svelte
@@ -1,34 +1,11 @@
 <script lang="ts">
-  import { _ } from 'svelte-i18n';
   import { SvaltiButton } from 'svalti';
 </script>
 
-<div>{$_('hello_world')}</div>
-
-<!-- <div class="grid">
-  {#each Array(100) as _, i}
-    <button use:SvaltiButton>
-      Button<br />#{i}
-    </button>
-  {/each}
-</div> -->
-<button use:SvaltiButton()>Clickable Button</button>
+<button use:SvaltiButton>Clickable Button</button>
+<button style="background-color: dodgerblue;" use:SvaltiButton>
+  Clickable Button
+</button>
 
 <style>
-  /* .grid {
-    display: grid;
-    grid-template-columns: repeat(10, 1fr);
-    grid-template-rows: repeat(10, 1fr);
-  } */
-  button {
-    padding: 0.3rem 0.6rem;
-    background-color: #444;
-    color: white;
-    font-family: inherit;
-    font-size: 1.1rem;
-    margin: 3px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
 </style>
--- src/lib/base.ts
+++ src/lib/base.ts
@@ -1,3 +1,5 @@
+import recursiveAssign from 'recursive-object-assign';
+
 function destroyer(node: HTMLElement): SvaltiNode {
   return {
     destroy() {
@@ -18,38 +20,63 @@ function getMouseInfo(node: HTMLElement, event: MouseEvent) {
   };
 }
 
-function applyStyle(node: HTMLElement, style: Partial<CSSStyleDeclaration>) {
+function applyStyle(
+  node: HTMLElement,
+  style: Partial<CSSStyleDeclaration>,
+  skipIfSet = false
+) {
   for (const key in style) {
+    if (node.style[key] && skipIfSet) continue;
     node.style[key] = style[key];
   }
 }
 
+const defaultGlassOptions = {
+  clickable: true,
+  disabled: false,
+  borderWidth: 2,
+  hoverRadius: 100,
+  clickDepth: 8,
+  clickDegrees: 10,
+  transitionDuration: 0.6,
+  shade: '255, 255, 255',
+  svaltiStyles: {
+    padding: '0.3rem 0.6rem',
+    backgroundColor: '#444',
+    color: 'white',
+    fontFamily: 'inherit',
+    fontSize: '1.1rem',
+    margin: '3px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  } as Partial<CSSStyleDeclaration>,
+};
+
+export type GlassOptions = typeof defaultGlassOptions;
+
 function applyGlassEffect(
   node: HTMLElement,
-  options?: {
-    clickable?: boolean;
-    disabled?: boolean;
-  }
+  customOptions: Partial<GlassOptions>
 ) {
-  const borderWidth = 2;
-  const hoverRadius = 100;
-  const clickDepth = 8;
-  const clickDegrees = 15;
-  const transitionDuration = 0.6;
-  const shade = '255, 255, 255';
-  const defaultShade = `rgba(${shade}, 0.3)`;
+  const options = { ...defaultGlassOptions };
+  recursiveAssign(options, customOptions);
+  applyStyle(node, options.svaltiStyles, true);
+  const defaultShade = `rgba(${options.shade}, 0.3)`;
   const defaultState: Partial<CSSStyleDeclaration> = {
     outline: 'none',
-    border: `${borderWidth}px solid transparent`,
-    backgroundPosition: `-${borderWidth}px -${borderWidth}px`,
+    border: `${options.borderWidth}px solid transparent`,
+    backgroundPosition: `-${options.borderWidth}px -${options.borderWidth}px`,
     backgroundSize: `
-      calc(100% + ${borderWidth * 2}px) calc(100% + ${borderWidth * 2}px)
+      calc(100% + ${options.borderWidth * 2}px)
+      calc(100% + ${options.borderWidth * 2}px)
     `,
     backgroundRepeat: 'no-repeat',
     transformOrigin: 'center center',
   };
   applyStyle(node, defaultState);
   let borderImage = 'none';
+  let holding = false;
   node.addEventListener('mouseenter', () => {
     const callback = (event: MouseEvent) => {
       const { x, y, width, height } = getMouseInfo(node, event);
@@ -57,23 +84,23 @@ function applyGlassEffect(
         inspired by:
         https://dev.to/jashgopani/windows-10-button-hover-effect-using-css-and-vanilla-js-1io4
       */
-      const r = (Math.max(width, height) * hoverRadius) / 100;
+      const r = (Math.max(width, height) * options.hoverRadius) / 100;
       borderImage = `
         radial-gradient(
           ${r}px ${r}px at ${x}px ${y}px,
-          rgba(${shade}, 0.7),
+          rgba(${options.shade}, 0.7),
           ${defaultShade}
-        ) 9 / ${borderWidth}px / 0px stretch
+        ) 9 / ${options.borderWidth}px / 0px stretch
       `;
       applyStyle(node, {
         backgroundImage: `
           radial-gradient(
             ${r}px ${r}px at ${x}px ${y}px,
-            rgba(${shade}, 0.25) 0%,
-            rgba(${shade}, 0.0) 100%
+            rgba(${options.shade}, 0.25) 0%,
+            rgba(${options.shade}, 0.0) 100%
           )
         `,
-        borderImage,
+        borderImage: holding ? 'none' : borderImage,
       });
     };
     window.addEventListener('mousemove', callback);
@@ -85,40 +112,50 @@ function applyGlassEffect(
       });
     });
   });
-  node.addEventListener('mousedown', (event) => {
-    const { x, y, width, height } = getMouseInfo(node, event);
-    let [xFactor, yFactor] = [2 * (x / width) - 1, 2 * (y / height) - 1];
-    if (Math.abs(xFactor) <= 0.2) xFactor = 0;
-    if (Math.abs(yFactor) <= 0.2) yFactor = 0;
-    const scale = clickDepth / Math.max(width, height);
-    const transformOrigin = 'center center';
-    applyStyle(node, {
-      transform: `rotate3d(${yFactor}, ${-xFactor}, 0, ${clickDegrees}deg) scale(${
-        1 - scale
-      })`,
-      transition: '0s',
-      transformOrigin,
-      borderImage: 'none',
-    });
-    const callback = () => {
+  if (options.clickable) {
+    node.addEventListener('mousedown', (event) => {
+      holding = true;
+      const { x, y, width, height } = getMouseInfo(node, event);
+      let [xFactor, yFactor] = [2 * (x / width) - 1, 2 * (y / height) - 1];
+      if (Math.abs(xFactor) + Math.abs(yFactor) !== 0) {
+        xFactor /= Math.abs(xFactor) + Math.abs(yFactor);
+        yFactor /= Math.abs(xFactor) + Math.abs(yFactor);
+      }
+      const scale = options.clickDepth / Math.max(width, height);
+      const transformOrigin = 'center center';
       applyStyle(node, {
-        transform: '',
-        transition: `transform ${transitionDuration}s`,
-        borderImage,
+        transform: `rotate3d(${yFactor}, ${-xFactor}, 0, ${
+          options.clickDegrees
+        }deg) scale(${1 - scale})`,
+        transition: '0s',
+        transformOrigin,
+        borderImage: 'none',
       });
-      window.removeEventListener('mouseup', callback);
-    };
-    window.addEventListener('mouseup', callback);
-  });
-  return;
+      const callback = () => {
+        holding = false;
+        applyStyle(node, {
+          transform: '',
+          transition: `transform ${options.transitionDuration}s`,
+          borderImage,
+        });
+        window.removeEventListener('mouseup', callback);
+      };
+      window.addEventListener('mouseup', callback);
+    });
+  }
 }
 
-export function SvaltiButton() {
+export function CustomSvaltiButton(options: Partial<GlassOptions> = {}) {
   return (node: HTMLElement): SvaltiNode => {
     applyGlassEffect(node, {
       clickable: true,
       disabled: false,
+      ...options,
     });
     return destroyer(node);
   };
+}
+
+export function SvaltiButton(node: HTMLElement): SvaltiNode {
+  return CustomSvaltiButton()(node);
 }
