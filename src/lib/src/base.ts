a06fae0ea963beafca53e85ed926b4d8eaf5c7ca
Kento Nishi <kento24gs@outlook.com>
2022-04-12 13:28:54 -0700

    register touch clicks

--- src/lib/src/base.ts
+++ src/lib/src/base.ts
@@ -37,7 +37,7 @@ export function destroyer(node: HTMLElement): ExioNode {
 
 export function getMouseInfo(
   node: HTMLElement,
-  event: MouseEvent
+  event: MouseEvent | Touch
 ): {
   x: number;
   y: number;
@@ -97,11 +97,6 @@ export function applyGlassEffect(
   customOptions: Partial<GlassOptions>
 ): void {
   const options = getMergedObject(defaultGlassOptions, customOptions);
-  console.log(
-    JSON.stringify(options, null, 2),
-    JSON.stringify(customOptions, null, 2),
-    JSON.stringify(defaultGlassOptions, null, 2)
-  );
   applyStyle(node, options.additionalStyles, true);
   const defaultShade = `rgba(${options.hoverRGB}, 0.3)`;
   const defaultState: Partial<CSSStyleDeclaration> = {
@@ -161,45 +156,52 @@ export function applyGlassEffect(
     });
   });
   if (options.clickable) {
-    node.addEventListener('mousedown', (event) => {
-      event.preventDefault();
-      clicking = true;
-      const { x, y, width, height } = getMouseInfo(node, event);
-      const [xFactor, yFactor] = [2 * (x / width) - 1, 2 * (y / height) - 1];
-      const p = options.clickPerspective * Math.min(width, height);
-      // if (Math.abs(xFactor) + Math.abs(yFactor) !== 0) {
-      //   xFactor /= Math.abs(xFactor) + Math.abs(yFactor);
-      //   yFactor /= Math.abs(xFactor) + Math.abs(yFactor);
-      // }
-      const transformOrigin = 'center center';
-      const scaling = 1 - options.clickScale / Math.max(width, height);
-      applyStyle(node, {
-        transform: `
+    const registerActive = (start = 'mousedown', end = 'mouseup') => {
+      node.addEventListener(start, (event: MouseEvent | TouchEvent) => {
+        event.preventDefault();
+        clicking = true;
+        const { x, y, width, height } = getMouseInfo(
+          node,
+          'touches' in event ? event.touches[0] : event
+        );
+        const [xFactor, yFactor] = [2 * (x / width) - 1, 2 * (y / height) - 1];
+        const p = options.clickPerspective * Math.min(width, height);
+        // if (Math.abs(xFactor) + Math.abs(yFactor) !== 0) {
+        //   xFactor /= Math.abs(xFactor) + Math.abs(yFactor);
+        //   yFactor /= Math.abs(xFactor) + Math.abs(yFactor);
+        // }
+        const transformOrigin = 'center center';
+        const scaling = 1 - options.clickScale / Math.max(width, height);
+        applyStyle(node, {
+          transform: `
           perspective(${p}px)
           rotateX(${-yFactor * options.clickDegrees}deg)
           rotateY(${xFactor * options.clickDegrees}deg)
           scale(${scaling})
         `,
-        transition: '0s',
-        transformOrigin,
-        borderImage: 'none',
-      });
-      const callback = () => {
-        clicking = false;
-        applyStyle(node, {
-          transform: `perspective(${p}px)`,
-          transition: `transform ${options.transitionDuration}s`,
-          borderImage,
+          transition: '0s',
+          transformOrigin,
+          borderImage: 'none',
         });
-        if (!hovering) {
+        const callback = () => {
+          clicking = false;
           applyStyle(node, {
-            borderImage: 'none',
-            backgroundImage: 'none',
+            transform: `perspective(${p}px)`,
+            transition: `transform ${options.transitionDuration}s`,
+            borderImage,
           });
-        }
-        window.removeEventListener('mouseup', callback);
-      };
-      window.addEventListener('mouseup', callback);
-    });
+          if (!hovering) {
+            applyStyle(node, {
+              borderImage: 'none',
+              backgroundImage: 'none',
+            });
+          }
+          window.removeEventListener(end, callback);
+        };
+        window.addEventListener(end, callback);
+      });
+    };
+    registerActive('mousedown', 'mouseup');
+    registerActive('touchstart', 'touchend');
   }
 }
