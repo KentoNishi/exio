19ef4a649b6150e3fe342c2062045cd350a144e8
Kento Nishi <kento24gs@outlook.com>
2022-04-28 20:13:27 -0700

    upgraded loader v0.1.7

--- src/lib/src/loading.ts
+++ src/lib/src/loading.ts
@@ -9,7 +9,7 @@ export function exioLoadingBar(node: HTMLElement): ExioNode {
       display: flex;
       align-items: center;
       overflow: hidden;
-      animation-duration: 0.8s;
+      animation-duration: 1s;
       animation-iteration-count: infinite;
       animation-timing-function: linear;
       animation-direction: normal;
@@ -17,22 +17,20 @@ export function exioLoadingBar(node: HTMLElement): ExioNode {
     }
     @keyframes exio-loading-bar {
       0% {
-        width: 0px;
-        left: 0px;
+        left: -100%;
       }
       50% {
-        width: 100%;
       }
       100% {
-        width: 0px;
-        right: 0px;
+        left: 100%;
       }
     }
     .${s.id}::before {
       content: '';
       position: absolute;
-      background-color: var(--exio-loading-bar-thumb-color);
+      background-image: linear-gradient(to left, var(--exio-loading-bar-thumb-color), transparent);
       height: 100%;
+      width: 100%;
       animation: exio-loading-bar;
       animation-duration: inherit;
       animation-iteration-count: inherit;
