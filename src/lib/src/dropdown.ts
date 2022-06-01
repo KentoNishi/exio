c31c3304d395906cfea47e7f8073f79adc48ff69
Kento Nishi <kento24gs@outlook.com>
2022-05-31 22:25:47 -0700

    dropdown (still overflows)

--- src/lib/src/dropdown.ts
+++ src/lib/src/dropdown.ts
@@ -10,19 +10,73 @@ export function exioDropdown(node: HTMLSelectElement): ExioNode {
       user-select: none;
     }
   `;
+  const dropdown = document.createElement('div');
+  const ds = styler(dropdown);
+  let rect = node.getBoundingClientRect();
+  let before = '';
   const onDown = (e: MouseEvent | TouchEvent) => {
+    before = node.style.getPropertyValue('transform');
     node.style.setProperty('transform', 'none', 'important');
-    const { width, height, top, left } = node.getBoundingClientRect();
-    node.style.removeProperty('transform');
+    rect = node.getBoundingClientRect();
     e.preventDefault();
     return;
   };
-  const onUp = (e: MouseEvent | TouchEvent) => {
-    return;
+  dropdown.tabIndex = 0;
+  const items: ExioNode[] = [];
+  const updateStyle = () => {
+    node.style.setProperty('transform', before, 'important');
+    const computed = getComputedStyle(node);
+    const transitionDuration = computed.getPropertyValue(
+      '--exio-transition-duration'
+    );
+    const color = computed.getPropertyValue('color');
+    const backgroundColor = computed.getPropertyValue('background-color');
+    const fontFamily = computed.getPropertyValue('font-family');
+    const fontSize = computed.getPropertyValue('font-size');
+    const padding = computed.getPropertyValue('padding');
+    ds.innerHTML = `
+      .${ds.id} {
+        position: fixed;
+        top: ${rect.bottom}px;
+        left: ${rect.left}px;
+        width: ${rect.width}px;
+        background-color: ${backgroundColor};
+        transition: opacity ${transitionDuration};
+        color: ${color};
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        padding: ${padding};
+        padding-left: 0px;
+        padding-right: 0px;
+        box-size: border-box;
+        opacity: 0;
+      }
+      .${ds.id}:focus {
+        opacity: 1;
+        outline: none;
+      }
+    `;
+    dropdown.innerHTML = '';
+    setTimeout(() => {
+      node.childNodes.forEach((child) => {
+        const item = document.createElement('div');
+        item.style.padding = padding;
+        item.style.boxSizing = 'border-box';
+        item.style.cursor = 'default';
+        item.textContent = child.textContent;
+        items.push(exioPointerEffect(item));
+        dropdown.appendChild(item);
+      });
+    }, 0);
+    dropdown.focus();
   };
+  updateStyle();
+  document.body.appendChild(dropdown);
   node.addEventListener('mousedown', onDown);
   node.addEventListener('touchstart', onDown);
-  node.addEventListener('mouseup', onUp);
-  node.addEventListener('touchend', onUp);
-  return destroyer(effect.destroy, s.remove);
+  node.addEventListener('mouseup', updateStyle);
+  node.addEventListener('touchend', updateStyle);
+  return destroyer(effect.destroy, s.remove, dropdown.remove, ds.remove, () => {
+    items.forEach((item) => item.destroy());
+  });
 }
