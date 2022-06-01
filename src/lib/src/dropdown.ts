e115f1e2059a652848a4dd256c0d103ba0b02cf1
Kento Nishi <kento24gs@outlook.com>
2022-05-31 19:54:43 -0700

    get dimensions of dropdown on mousedown

--- src/lib/src/dropdown.ts
+++ src/lib/src/dropdown.ts
@@ -11,12 +11,18 @@ export function exioDropdown(node: HTMLSelectElement): ExioNode {
     }
   `;
   const onDown = (e: MouseEvent | TouchEvent) => {
+    node.style.setProperty('transform', 'none', 'important');
+    const { width, height, top, left } = node.getBoundingClientRect();
+    node.style.removeProperty('transform');
+    e.preventDefault();
+    return;
+  };
+  const onUp = (e: MouseEvent | TouchEvent) => {
     return;
   };
   node.addEventListener('mousedown', onDown);
   node.addEventListener('touchstart', onDown);
-  return destroyer(effect.destroy, s.remove, () => {
-    node.removeEventListener('mousedown', onDown);
-    node.removeEventListener('touchstart', onDown);
-  });
+  node.addEventListener('mouseup', onUp);
+  node.addEventListener('touchend', onUp);
+  return destroyer(effect.destroy, s.remove);
 }
