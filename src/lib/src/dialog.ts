afe390d464ec96d71331966f6c8d8b0d2f2f4811
Kento Nishi <kento24gs@outlook.com>
2022-06-15 22:43:05 -1000

    0.2 dialog

--- src/lib/src/dialog.ts
+++ src/lib/src/dialog.ts
@@ -28,7 +28,7 @@ export function exioDialog(node: HTMLDialogElement): ExioNode {
     }
     .${s.id} {
       --exio-transition-duration: 0.4s;
-      --exio-backdrop-color: rgba(128, 128, 128, 0.1);
+      --exio-backdrop-color: rgba(128, 128, 128, 0.2);
       border-radius: 0px;
       border: 0px solid transparent;
     }
