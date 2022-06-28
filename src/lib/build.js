88db90d15c4329b3d62f0d2090c78380a4f9acee
Kento Nishi <kento24gs@outlook.com>
2022-06-27 19:30:24 -0700

    sort package.json

--- src/lib/build.js
+++ src/lib/build.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 import { copyFileSync, mkdirSync, existsSync } from 'fs';
 import { execSync } from 'child_process';
 
