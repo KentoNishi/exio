b02aa69474500c638ba9752912c2386a12d9c448
r2dev2 <ronak.badhe@gmail.com>
2022-09-17 15:06:28 -0700

    enable source map for production

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    sourcemap: true
+  },
   server: {
     port: 5000,
   },
