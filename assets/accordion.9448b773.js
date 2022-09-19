8756d187d24cfa546f9475f0cc7490b17fc8239d
KentoNishi <KentoNishi@users.noreply.github.com>
2022-09-19 05:46:02 +0000

    deploy: 39e4658336d9e4d650650c4e1ee77563ad6ff4e5

--- assets/accordion.9448b773.js
+++ assets/accordion.9448b773.js
@@ -1,4 +1,4 @@
-import{e as p,c as g,a as T,s,d as y,t as H}from"./index.4c9ae33e.js";function v(o){const l=p(o);g();const t=o.querySelector("summary"),d=T(t,{borderStyle:"reactive"}),i=s(t);i.innerHTML=`
+import{e as p,c as g,a as T,s,d as y,t as H}from"./index.60c68b62.js";function v(o){const l=p(o);g();const t=o.querySelector("summary"),d=T(t,{borderStyle:"reactive"}),i=s(t);i.innerHTML=`
     .${i.id} {
       cursor: default;
       user-select: none;
@@ -46,4 +46,4 @@ import{e as p,c as g,a as T,s,d as y,t as H}from"./index.4c9ae33e.js";function v
             max-height: ${f()}px;
           }
         `,n=setTimeout(()=>{e.innerHTML=""},c())},0))};return t.addEventListener("click",m),y(()=>{d.destroy(),i.remove(),a.remove(),e.remove(),l.destroy(),t.removeEventListener("click",m)})}export{v as e};
-//# sourceMappingURL=accordion.112cec09.js.map
+//# sourceMappingURL=accordion.9448b773.js.map
