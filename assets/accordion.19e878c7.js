7db93d61bd7fd46e590c2b6652312f24837de60a
KentoNishi <KentoNishi@users.noreply.github.com>
2023-01-09 00:43:39 +0000

    deploy: 35657f8aebffeae35f62432502d2035071554368

--- assets/accordion.19e878c7.js
+++ assets/accordion.19e878c7.js
@@ -1,4 +1,4 @@
-var $=Object.defineProperty;var p=Object.getOwnPropertySymbols;var H=Object.prototype.hasOwnProperty,v=Object.prototype.propertyIsEnumerable;var x=(e,t,o)=>t in e?$(e,t,{enumerable:!0,configurable:!0,writable:!0,value:o}):e[t]=o,c=(e,t)=>{for(var o in t||(t={}))H.call(t,o)&&x(e,o,t[o]);if(p)for(var o of p(t))v.call(t,o)&&x(e,o,t[o]);return e};import{e as L,c as M,a as k,s as m,u as w,d as E,p as S,t as b}from"./index.64cea668.js";const C=c({transitionDuration:{prop:"--exio-slow-transition-duration",val:""}},S);function z(e,t={}){const o=L(e);M();const r=e.querySelector("summary"),h=k(r,{borderStyle:"reactive"}),n=m(r);n.innerHTML=`
+var $=Object.defineProperty;var p=Object.getOwnPropertySymbols;var H=Object.prototype.hasOwnProperty,v=Object.prototype.propertyIsEnumerable;var x=(e,t,o)=>t in e?$(e,t,{enumerable:!0,configurable:!0,writable:!0,value:o}):e[t]=o,c=(e,t)=>{for(var o in t||(t={}))H.call(t,o)&&x(e,o,t[o]);if(p)for(var o of p(t))v.call(t,o)&&x(e,o,t[o]);return e};import{e as L,c as M,a as k,s as m,u as w,d as E,p as b,t as S}from"./index.8caf91b9.js";const C=c({transitionDuration:{prop:"--exio-slow-transition-duration",val:""}},b);function z(e,t={}){const o=L(e);M();const r=e.querySelector("summary"),h=k(r,{borderStyle:"reactive"}),n=m(r);n.innerHTML=`
     .${n.id} {
       cursor: default;
       user-select: none;
@@ -25,7 +25,7 @@ var $=Object.defineProperty;var p=Object.getOwnPropertySymbols;var H=Object.prot
       transition: max-height var(--exio-slow-transition-duration);
       overflow: hidden;
     }
-  `;const g=e.querySelector("*:not(summary)"),i=m(e),s=m(r);let a;const l=y=>{a&&clearTimeout(a);const T=getComputedStyle(r),d=()=>b(T.getPropertyValue("--exio-slow-transition-duration")),u=()=>r.offsetHeight+g.offsetHeight;e.open?(y.preventDefault(),i.innerHTML=`
+  `;const g=e.querySelector("*:not(summary)"),i=m(e),s=m(r);let a;const l=y=>{a&&clearTimeout(a);const T=getComputedStyle(r),d=()=>S(T.getPropertyValue("--exio-slow-transition-duration")),u=()=>r.offsetHeight+g.offsetHeight;e.open?(y.preventDefault(),i.innerHTML=`
         .${i.id} {
           max-height: ${u()}px;
         }
@@ -50,4 +50,4 @@ var $=Object.defineProperty;var p=Object.getOwnPropertySymbols;var H=Object.prot
             max-height: ${u()}px;
           }
         `,a=setTimeout(()=>{i.innerHTML=""},d())},0))};return r.addEventListener("click",l),c(c({},w(t,e,C)),E(()=>{h.destroy(),n.remove(),f.remove(),i.remove(),o.destroy(),r.removeEventListener("click",l)}))}export{z as e};
-//# sourceMappingURL=accordion.04be29df.js.map
+//# sourceMappingURL=accordion.19e878c7.js.map
